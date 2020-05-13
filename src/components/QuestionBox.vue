4ed20172317cbee792be85eda61b81b901109ecd
Sekar Larasati Muslimah <13517114@std.stei.itb.ac.id>
2020-05-13 16:16:15 +0700

    add question box

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <b-jumbotron>
+            <template v-slot:lead>
+            {{currQuestion.question}}
+            </template>
+
+            <hr class="my-4">
+
+            <p>
+            List of Answer
+            </p>
+
+            <b-button variant="primary" href="#">Submit</b-button>
+            <b-button @click="next" variant="success" href="#">Next</b-button>
+        </b-jumbotron>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        currQuestion: Object,
+        next: Function
+    }
+}
+</script>
